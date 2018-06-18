01f6c43452f338d3a75476ac7cd56644c404b53f
OA Wu <comdan66@gmail.com>
2018-06-18 20:20:45 +0800

    🤪 Finish! 完成啦，來去洗洗睡！

--- js/public.js
+++ js/public.js
@@ -6,5 +6,19 @@
  */
  
 $(function () {
+  $box = $('#box');
+  $box.find ('.close').click (function () {
+    $box.removeClass ('show');
+  });
+  $box.find ('.add-specification').click (function () {
+    var $tmp = $(this).prev ().clone ();
+    $tmp.find ('input').val ('');
+    $tmp.find ('select > option').first ().prop ('selected');
+    $tmp.insertBefore ($(this));
+  });
 
+  $('._ic').imgLiquid ({ verticalAlign:'center' });
+  $('.forbox').click (function () {
+    $box.addClass ('show');
+  });
 });
